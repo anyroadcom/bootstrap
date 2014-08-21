5581e9d7078df77ccbfc81e5903630d57ac03b89
Bogdan Dumitru <dumbogdan@gmail.com>
2014-08-21 13:13:59 +0300

    Add bootstrap tooltips

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * In order to register all the jquery helpers
+ * we need to require them.
+ * They don't export anything because they attach to jquery.
+ * So we don't export anything either.
+ */
+
+require('./js/tooltips.js');
